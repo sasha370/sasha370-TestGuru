f28447848572ae70429fce31982f0e0c2a427e07
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-11-18 22:45:21 +0300

    rebuild

--- app/controllers/pages_controller.rb
+++
@@ -1,8 +0,0 @@
-class PagesController < ApplicationController
-
-  def about
-  end
-
-  def author
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
 
-  get '/about', to: 'pages#about'
-  get '/about/author', to: 'pages#author'
+
 end
