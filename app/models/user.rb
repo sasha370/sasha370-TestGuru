bb4104012623efc33792329dedd5f44de71e2170
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-11-20 17:24:50 +0300

    add SQL for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
 
   def passed_tests_by_level(level)
     tests.where(level: level)
+    # Test.joins('JOIN user_tests as ut ON ut.test_id = tests.id').where('tests.level = ? AND ut.user_id = ?', level, self.id)
   end
-
 end
