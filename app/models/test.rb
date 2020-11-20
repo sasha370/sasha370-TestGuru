f8c06630387a5e5b30e1e30bcab63d0f27ca5f16
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-11-20 14:32:46 +0300

    add method to Tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  def self.find_by_category(category_name)
+    joins(:category).where("categories.title = ?", category_name).order(title: :desc).pluck(:title)
+  end
 end
