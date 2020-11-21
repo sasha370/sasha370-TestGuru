83e815fb552edeaa7a40e7d3a11ea01a0f0d877e
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-11-21 13:24:48 +0300

    add user_id to Tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_175813) do
+ActiveRecord::Schema.define(version: 2020_11_21_101927) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: true, null: false
@@ -41,7 +41,9 @@ ActiveRecord::Schema.define(version: 2020_11_20_175813) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "tests_users", force: :cascade do |t|
@@ -61,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_175813) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
   add_foreign_key "tests_users", "tests"
   add_foreign_key "tests_users", "users"
 end
