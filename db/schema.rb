ce9d82064c3d6800553c207479ef83affe6b252a
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-11-21 17:01:14 +0300

    add email field to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_101927) do
+ActiveRecord::Schema.define(version: 2020_11_21_135846) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: true, null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_11_21_101927) do
     t.string "password", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
   end
 
   add_foreign_key "answers", "questions"
