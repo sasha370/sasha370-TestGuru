27afd8fa0763b33a61bffa87355b49e2792b1b52
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-11-24 14:42:52 +0300

    add Answers scaffold

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,50 @@
+class AnswersController < ApplicationController
+  before_action :set_answer, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @answers = Answer.all
+  end
+
+  def show
+  end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def edit
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+
+    if @answer.save
+      redirect_to @answer, notice: 'Answer was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @answer.update(answer_params)
+      redirect_to @answer, notice: 'Answer was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @answer.destroy
+    redirect_to answers_url, notice: 'Answer was successfully destroyed.'
+  end
+
+  private
+
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
+
+  def answer_params
+    params.fetch(:answer, {})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :answers
   root to: 'tests#index'
 
   resources :tests do
