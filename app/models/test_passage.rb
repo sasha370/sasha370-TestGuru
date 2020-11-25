520512f00433b7690e13f4c066ae39142b06429c
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-11-25 12:24:15 +0300

    add bootstrape progressbar

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,7 +5,9 @@ class TestPassage < ApplicationRecord
   before_validation :set_first_question, on: :create
   before_update :before_update_set_next_question
 
-  PASS_TEST_PERCENT = 85
+  def before_update_set_next_question
+    self.current_question = next_question
+  end
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
@@ -16,10 +18,6 @@ class TestPassage < ApplicationRecord
     current_question.nil?
   end
 
-  def before_update_set_next_question
-    self.current_question = next_question
-  end
-
   def current_question_index
     test.questions.order(:id).where('id < ?', current_question.id).size + 1
   end
@@ -28,15 +26,21 @@ class TestPassage < ApplicationRecord
     test.questions.count
   end
 
-  def total_progress_in_percent
+  def test_completed_percent
+    (current_question_index / total_questions_in_test.to_f * 100).to_i
+  end
+
+  def correct_answers_in_percent
     (correct_questions.to_f / total_questions_in_test.to_f * 100).to_i
   end
 
   def success?
-    total_progress_in_percent >= PASS_TEST_PERCENT
+    correct_answers_in_percent >= PASS_TEST_PERCENT
   end
 
   private
+
+  PASS_TEST_PERCENT = 85
 
   def set_first_question
     self.current_question = test.questions.first if test.present?
