fcb4ea54719ab930c2c236323b56a29b032d9119
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-11-29 14:54:53 +0300

    locale not change if same

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def default_url_options
-    { lang: I18n.locale }
+      { lang: I18n.locale } if params[:lang] != I18n.default_locale
   end
 
   def after_sign_in_path_for(user)
