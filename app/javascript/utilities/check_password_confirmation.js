b4c801175999a072fb72a305357592eec2b9f2ca
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-12-01 14:43:00 +0300

    add js for password field

---
+++ app/javascript/utilities/check_password_confirmation.js
@@ -0,0 +1,33 @@
+document.addEventListener('turbolinks:load', function () {
+  // Get form block whit password
+  form = document.querySelector('.need-validation')
+  if (form) {
+    var passwordField = this.querySelector('.password_field')
+    var confirmationField = this.querySelector('.confirmation_field')
+
+    // listen to an event in any field
+    if (passwordField && confirmationField) {
+      passwordField.addEventListener('input', validateFields)
+      confirmationField.addEventListener('input', validateFields)
+    }
+
+    function validateFields() {
+      var password = passwordField.value
+      var confirmation = confirmationField.value
+
+      if (confirmation !== '') {
+        if (password !== confirmation) {
+          confirmationField.classList.remove('is-valid')
+          confirmationField.classList.add('is-invalid')
+        } else {
+          confirmationField.classList.remove('is-invalid')
+          confirmationField.classList.add('is-valid')
+        }
+      } else {
+        // cleaUp field`s status if delete all chars
+        confirmationField.classList.remove('is-invalid')
+        confirmationField.classList.remove('is-valid')
+      }
+    }
+  }
+})
