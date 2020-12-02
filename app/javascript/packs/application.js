1ccd03d91cdb6b6a83db8d4b02c017660852f43a
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-12-02 14:29:02 +0300

    add progressbar to Test-passage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("channels")
 import "utilities/sorting"
 import "utilities/check_password_confirmation"
 import "utilities/form_inline"
+import "utilities/progress_bar"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
