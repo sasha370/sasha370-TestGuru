dee795910803c19721e2823795e4f681bec99ddd
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-12-03 15:29:56 +0300

    repaire FeedbackMailer

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,11 +1,12 @@
 class FeedbackMailer < ApplicationMailer
+  before_action :set_admin
 
   def feedback(feedback)
     @title = feedback[:title]
     @description = feedback[:description]
     @user = feedback[:user]
 
-    mail to: set_admin,
+    mail to: @admin,
          subject: "#{@title}"
   end
 
